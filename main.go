7f12542afc5aa087dee5534fd84ade76d131f72c
web3wanhong <wanhong@web3.com>
2023-11-27 15:49:51 +0800

    -dir

--- main.go
+++ main.go
@@ -14,13 +14,14 @@ var (
 	postNonce     uint
 	postPow       uint64
 	indicesString string
+	dir           string
 )
 
 func parseFlags() {
 	flag.UintVar(&postNonce, "postNonce", 0, "post proof nonce")
 	flag.Uint64Var(&postPow, "postPow", 0, "post proof pow")
 	flag.StringVar(&indicesString, "postIndices", "", "post proof indices")
-
+	flag.StringVar(&dir, "dir", "./", "post.bin store dir")
 	flag.Parse()
 }
 
@@ -34,7 +35,7 @@ func main() {
 	fmt.Printf("indices: %v", indices)
 
 	post := &shared.Proof{Nonce: uint32(postNonce), Indices: indices, Pow: postPow}
-	if err := activation.SavePost("./", (*types.Post)(post)); err != nil {
+	if err := activation.SavePost(dir, (*types.Post)(post)); err != nil {
 		fmt.Printf("failed to save initial post: %v", err)
 	}
 	fmt.Printf("quit.")
