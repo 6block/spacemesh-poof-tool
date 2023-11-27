88027139543f20e04d32f0df0d9c955b1c7f5d2d
scuwan <215587407@qq.com>
2023-11-27 15:25:12 +0800

    2

--- go.mod
+++ go.mod
@@ -156,3 +156,5 @@ require (
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.2.1 // indirect
 )
+
+replace github.com/spacemeshos/go-spacemesh v1.2.6 => github.com/6block/go-sapcemesh v1.2.7
--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"flag"
 	"fmt"
 
+	"github.com/spacemeshos/go-spacemesh/activation"
+	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/post/shared"
-    "github.com/spacemeshos/go-spacemesh/activation"
 )
 
 var (
@@ -18,23 +19,23 @@ var (
 func parseFlags() {
 	flag.UintVar(&postNonce, "postNonce", 0, "post proof nonce")
 	flag.Uint64Var(&postPow, "postPow", 0, "post proof pow")
-	flag.StringVar(&indicesString, "postIndices", "", "post proof indices"
+	flag.StringVar(&indicesString, "postIndices", "", "post proof indices")
 
 	flag.Parse()
 }
 
 func main() {
 	parseFlags()
-	
-    indices, err := hex.DecodeString(indicesString)
-    if err != nil {
-        fmt.Printf("invalid commitmentAtxId: %v", err)
-    }
-    fmt.Printf("indices: %v", indices)
 
-    post := &shared.Proof{Nonce: uint32(postNonce), Indices: indices, Pow: postPow}
-    if err := activation.savePost("./", post); err != nil {
-		b.log.With().Warning("failed to save initial post: %w", log.Err(err))
+	indices, err := hex.DecodeString(indicesString)
+	if err != nil {
+		fmt.Printf("invalid commitmentAtxId: %v", err)
+	}
+	fmt.Printf("indices: %v", indices)
+
+	post := &shared.Proof{Nonce: uint32(postNonce), Indices: indices, Pow: postPow}
+	if err := activation.SavePost("./", (*types.Post)(post)); err != nil {
+		fmt.Printf("failed to save initial post: %v", err)
 	}
 	fmt.Printf("quit.")
 }
